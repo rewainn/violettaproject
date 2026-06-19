63e964ea3cedea383eb9b81e70f9e29955b4e625
rewainn <danyaaasd111@gmail.com>
2026-06-20 00:01:25 +0300

    @vercel/speed-insights

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Layout } from "./components/Layout";
 import { Home } from "./pages/Home";
 import { Calculator } from "./pages/Calculator";
@@ -19,6 +20,7 @@ export default function App() {
         </Route>
       </Routes>
       <Analytics />
+      <SpeedInsights />
     </BrowserRouter>
   );
 }
